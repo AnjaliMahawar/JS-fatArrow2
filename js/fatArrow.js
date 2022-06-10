d286797d39e3e69e9cf320a9c00edba83bfc1ae3
Anjali MahawR <anjalimahawar454@gmail.com>
2022-06-10 06:31:11 -0400

    fatArrow

---
+++ js/fatArrow.js
@@ -0,0 +1,8 @@
+let x = myFunction=(n)=>{
+ console.log('good morning '+n)
+ return n;
+}
+myFunction('anjali')
+
+let r2=myFunction('priya')
+console.log(r2)
---
+++ js/fatArrow2.js
@@ -0,0 +1,7 @@
+let x = myFunc=(a)=>{
+    //console.log('hey..'+a+ " Are you oky?")
+    return a;
+}
+
+let r = myFunc('priya');
+console.log('hey '+r +' are you ok')
